e4ab6e62f576ce8d37210327176840f531845688
Victor Rodrigues <contato@victormkt.com.br>
2021-10-06 14:42:28 -0300

    inclusão de novos arquivos

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+// instancia jquery e evita conflitos
+//jQuery(function ($){
+
+$(document).ready(function ()){
+
+    $('.owl-carousel').owlCarousel();
+
+    let titulos = $('h4') //tag
+
+    let items = $('.featureditem') //class
+
+    let destaques = $('#featured') // id
+
+})
