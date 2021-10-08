2f3de685760a33cec3a9b5b95a4868a1f017e0ac
Victor Rodrigues <contato@victormkt.com.br>
2021-10-08 14:03:03 -0300

    validação de campos vazios de fomulario no modal

--- js/custom.js
+++ js/custom.js
@@ -2,45 +2,44 @@
 //jQuery(function ($){
 
 $(document).ready(function () {
+  $(".owl-carousel").owlCarousel();
 
-    $('.owl-carousel').owlCarousel();
+  let titulos = $("h4"); //tag
 
-    let titulos = $('h4') //tag
+  let items = $(".featureditem"); //class
 
-    let items = $('.featureditem') //class
+  let destaques = $("#featured"); // id
 
-    let destaques = $('#featured') // id
+  console.log(titulos.first());
 
-    console.log(titulos.first());
+  //Configuração de produtos
 
-    //Configuração de produtos
+  // $('.featured-item a').addClass('btn btn-dark stretch-link');
 
-    // $('.featured-item a').addClass('btn btn-dark stretch-link');
+  $(".featured-item:nth(0) h4")
+    .append(" ")
+    .append('<span class="badge bg-primary">Novo</span>');
+  // $('.featured-item:first h4').start('<span class="badge bg-secondary">Novo</span>')
+  // $('.featured-item:first h4').html('<span class="badge bg-secondary">Novo</span>')
+  // $('.featured-item:first h4').addClass('active')
+  // $('.featured-item:first h4').removeClass('active')
+  // $('.featured-item:first h4').toogleClass('active')
+  // $('.featured-item:first h4').hide()
+  // $('.featured-item:first h4').show()
+  // $('.featured-item:first h4').fadeIn(2000)
+  // $('.featured-item:first h4').fadeOut()
+  // $('.featured-item:first h4').css('color','#f00')
 
-    $('.featured-item:nth(0) h4').append(' ').append('<span class="badge bg-primary">Novo</span>')
-    // $('.featured-item:first h4').start('<span class="badge bg-secondary">Novo</span>')
-    // $('.featured-item:first h4').html('<span class="badge bg-secondary">Novo</span>')
-    // $('.featured-item:first h4').addClass('active')
-    // $('.featured-item:first h4').removeClass('active')
-    // $('.featured-item:first h4').toogleClass('active')
-    // $('.featured-item:first h4').hide()
-    // $('.featured-item:first h4').show()
-    // $('.featured-item:first h4').fadeIn(2000)
-    // $('.featured-item:first h4').fadeOut()
-    // $('.featured-item:first h4').css('color','#f00')
+  // $('.featured-item h4').dblclick(function () {
+  //     $(this).css({
+  //         'color': '#f00',
+  //         'background': '#ff0',
+  //         'font-weight': '100'
+  //     })
+  // })
 
-
-
-    // $('.featured-item h4').dblclick(function () {
-    //     $(this).css({
-    //         'color': '#f00',
-    //         'background': '#ff0',
-    //         'font-weight': '100'
-    //     })
-    // })
-
-    //  Manopulação de eventos
-    /*
+  //  Manopulação de eventos
+  /*
        $('featured-item a').on('blur', function (event) {
            event.preventDefault();
            alert('Produto esgotado');
@@ -60,7 +59,7 @@ $(document).ready(function () {
    
            })*/
 
-    /*
+  /*
     * Animações
     
     const duracao = 1000 // equivale a 1 segundo
@@ -72,36 +71,91 @@ $(document).ready(function () {
         .toogle(duracao)
         .toogle(duracao)*/
 
-    $('#form-submit').on('click', function (e) {
-        e.preventDefault()
+  $("#form-submit").on("click", function (e) {
+    e.preventDefault();
 
-        if ($('#email').val() != "") {
-
-            $('#email').animate({
-                opacity: "hide",
-                top: "-50"
-            }, 500, function () {
-                console.log($(this).val())
-            })
+    if ($("#email").val() != "") {
+      $("#email").animate(
+        {
+          opacity: "hide",
+          top: "-50",
+        },
+        500,
+        function () {
+          console.log($(this).val());
         }
-    });
+      );
+    }
+  });
+
+  /*
+   *  Ouvinte de eventos .nav-modal-open
+   */
 
 
-    /*
-     *  Ouvinte de eventos .nav-modal-open
-    */
-
-    $('.nav-modal-open').on('click', function (e) {
-        e.preventDefault();
-        let elem = $(this).attr('rel')
-
-        $('.modal-body').html($('#'+elem).html())
-        $('.modal-header h5.modal-title').html($(this).text())
 
 
-        let myModal = new bootstrap.Modal($('#modelId'))
-        myModal.show()
+  $(".nav-modal-open").on("click", function (e) {
+    e.preventDefault();
+    let elem = $(this).attr("rel");
 
-    })
+    $(".modal-body").html($("#" + elem).html());
+    $(".modal-header h5.modal-title").html($(this).text());
+
+    let myModal = new bootstrap.Modal($("#modelId"));
+    myModal.show();
+  });
+
+
+/*
+  * TODO: Incrementar a validação
+  * - chegar se o nome é válido (com mais de 2 caracteres)
+  * - checar se o e-mail é valido com ao menos um @ e "."
+  */
+ 
+  function validate(elem) {
+    if (elem.val() == "") {
+      console.log('O campo do ' + elem.attr('name') + ' é obrigatório')
+
+      elem.addClass('invalid')
+
+      return false
+    } else {
+      elem.removeClass('invalid')
+    }
+  }
+
+
+  $('body').on('submit', '.modal-body .form', function (e) {
+
+    e.preventDefault();
+
+    const inputName = $('#nome')
+    const inputEmail = $('#email')
+
+    validate(inputName)
+    validate(inputEmail)
+
+    if (inputEmail.hasClass('invalid') || inputName.hasClass('invalid')) {
+      console.log('verificar campos obrigatórios')
+      return false
+
+    } else {
+      $(this).submit()
+    }
+
+  })
+
+
+
+
+  $('body').on('blur', '#nome', function () {
+    validate($(this))
+  })
+
+  $('body').on('blur', '#email', function () {
+    validate($(this))
+  })
+
 
 })
